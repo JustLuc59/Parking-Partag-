0b56f340dc140e0900eca6dcfea01055ed81eb47
bellepenisto59 <164348029+bellepenisto59@users.noreply.github.com>
2024-03-26 11:50:15 +0100

    Add files via upload

--- code qrcode.py
+++ code qrcode.py
@@ -1,15 +1,15 @@
-User
 import qrcode
-
+from PIL import Image
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
-                   box_size=20,
-                   border=2)
+                   box_size=50,
+                   border=1)
 
 qr.add_data("caca pipi")
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("advanced.png")
+
+img.show()
 
