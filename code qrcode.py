6e1213a8598a3ba1058d8531fc3f16547b8897c0
bellepenisto59 <164348029+bellepenisto59@users.noreply.github.com>
2024-03-26 11:48:14 +0100

    Add files via upload

---
+++ code qrcode.py
@@ -0,0 +1,15 @@
+User
+import qrcode
+
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_L,
+                   box_size=20,
+                   border=2)
+
+qr.add_data("caca pipi")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("advanced.png")
+
