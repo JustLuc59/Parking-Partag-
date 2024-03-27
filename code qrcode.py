a8bd8179186ee771299cc167bcb190fa807afd46
bellepenisto59 <164348029+bellepenisto59@users.noreply.github.com>
2024-03-27 11:00:54 +0100

    Add files via upload

--- code qrcode.py
+++ code qrcode.py
@@ -1,15 +1,16 @@
 import qrcode
-from PIL import Image
+import random
+import string
+
+longueur_chaine = 10
+donnees_aleatoires = ''.join(random.choices(string.ascii_letters + string.digits, k=longueur_chaine))
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=50,
                    border=1)
-
-qr.add_data("caca pipi")
+qr.add_data(donnees_aleatoires)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-
 img.show()
-
